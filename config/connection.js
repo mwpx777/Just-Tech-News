a66fb528c81588bc3cc16c8eac80184ba8d4e7e5
MWPX777 <mwpx777@gmail.com>
2021-03-09 07:15:13 -0600

    connect to JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,26 @@
 // import the Sequelize constructor from the library
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 
-require('dotenv').config();
+// require('dotenv').config();
 
-// create connection to our database, pass in your MySQL info 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+// // create connection to our database, pass in your MySQL info 
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+// });
 
-module.exports = sequelize;
+// module.exports = sequelize;
+
+let sequelize;
+// this will first connect to JAWSDB, or else connect to localhost
+if (process.env.JAWSDB_URL){
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+}else{
+    sequalize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW,{
+        host: 'localhost',
+        dialect: 'mysql2',
+        port: 3306
+    });
+}
+
