502922b5bcf27c863339dca01262b4c5c9d81597
MWPX777 <mwpx777@gmail.com>
2021-03-12 07:24:38 -0600

    pushing to main

--- config/connection.js
+++ config/connection.js
@@ -12,15 +12,32 @@
 
 // module.exports = sequelize;
 
+// let sequelize;
+// // this will first connect to JAWSDB, or else connect to localhost
+// if (process.env.JAWSDB_URL){
+//     sequelize = new Sequelize(process.env.JAWSDB_URL);
+// }else{
+//     sequalize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW,{
+//         host: 'localhost',
+//         dialect: 'mysql2',
+//         port: 3306
+//     });
+// }
+
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
 let sequelize;
-// this will first connect to JAWSDB, or else connect to localhost
-if (process.env.JAWSDB_URL){
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-}else{
-    sequalize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW,{
-        host: 'localhost',
-        dialect: 'mysql2',
-        port: 3306
-    });
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
 }
 
+module.exports = sequelize;
