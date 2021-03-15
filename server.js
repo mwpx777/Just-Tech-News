2b89353d55f843620e5394fc09541f5ff9055a48
MWPX777 <mwpx777@gmail.com>
2021-03-15 10:27:06 -0500

    finished with 14.4

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const sequelize = require('./config/connection');
 const path = require('path');
 //  these are required for handlebars
 const exphbs= require('express-handlebars');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({helpers});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
